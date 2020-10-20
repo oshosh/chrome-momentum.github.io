8683ae8ea9933ca806b7f835045975760c36f5c8
osh <dhtpgus7@gmail.com>
2020-10-20 11:13:12 +0900

    commit

--- bg.js
+++ bg.js
@@ -3,11 +3,17 @@ const IMG_NUMBER = 1
 
 function paintImage(imageNumber) {
     const image = new Image()
-    image.src = `../images/${imageNumber + 1}.jpg`
+    //image.src = `images/${imageNumber + 1}.jpg`
+    image.src = `https://raw.githubusercontent.com/oshosh/chrome-momentum.github.io/master/Images/1.jpg`
     debugger
     image.classList.add('bgImage')
     body.prepend(image)
     // image.addEventListener('loadend',)
+
+    const myImage = document.querySelector(".bgImage");
+    myImage.onerror = function () {
+        this.src = "http://placehold.it/150x150";
+    }
 }
 
 function getRandom() {
